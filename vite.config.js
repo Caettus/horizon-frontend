e8b97a1fa37f17b8c90a11333e6615e911df79a4
Caettus <vincentscheffer2@gmail.com>
2025-05-17 11:30:09 +0200

    update aan vite config

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:50611',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
